748544a6a88908f43031a45d56e9c144b39eed6f
Sergei Pechinkin <pechinkinsergey@yandex.ru>
2023-04-09 14:10:21 +0400

    feat: add slider 1280 768 320 pets page (Sun, Apr 9, 2023 02:10:00 PM)

--- shelter/scripts/index-pets.js
+++ shelter/scripts/index-pets.js
@@ -6,6 +6,14 @@ const sectionPetsFive = document.querySelector('#pets-five');
 const sectionPetsSix = document.querySelector('#pets-six');
 const sectionPetsSeven = document.querySelector('#pets-seven');
 const sectionPetsEight = document.querySelector('#pets-eight');
+const sectionPetsNine = document.querySelector('#pets-nine');
+const sectionPetsTen = document.querySelector('#pets-ten');
+const sectionPetsEleven = document.querySelector('#pets-eleven');
+const sectionPetsTwelve = document.querySelector('#pets-twelve');
+const sectionPetsThirteen = document.querySelector('#pets-thirteen');
+const sectionPetsFourteen = document.querySelector('#pets-fourteen');
+const sectionPetsFifteen = document.querySelector('#pets-fifteen');
+const sectionPetsSixteen = document.querySelector('#pets-sixteen');
 
 
 const sliderPets = document.querySelector('.slider-pets');
@@ -63,121 +71,203 @@ const renderCard = (template, data) => {
   template.append(createCard(data));
 }
 
-const renderCardLeft = (data) => {
-}
-
-const renderCardRight = (data) => {
-}
-
-
-
-// dataPets.forEach(elem => {
-//     renderCard(elem)
-// });
-
-// setOfCards.forEach(item => {
-//   item.forEach(elem => renderCard(sectionsArray[count], elem));
-// })
-
 setOfCards[0].forEach(elem => renderCard(sectionPetsOne, elem));
 setOfCards[1].forEach(elem => renderCard(sectionPetsTwo, elem));
 setOfCards[2].forEach(elem => renderCard(sectionPetsThree, elem));
 setOfCards[3].forEach(elem => renderCard(sectionPetsFour, elem));
 setOfCards[4].forEach(elem => renderCard(sectionPetsFive, elem));
 setOfCards[5].forEach(elem => renderCard(sectionPetsSix, elem));
-// // setOfCards[6].forEach(elem => renderCard(sectionPetsSeven, elem));
-// setOfCards[1].sort(() => Math.random() - 0.5).forEach(elem => renderCard(sectionPetsSeven, elem));
-// setOfCards[2].sort(() => Math.random() - 0.5).forEach(elem => renderCard(sectionPetsEight, elem));
-// setOfCards[3].forEach(elem => renderCard(sectionPetsTwo, elem));
-// setOfCards[4].forEach(elem => renderCard(sectionPetsTwo, elem));
-// setOfCards[5].forEach(elem => renderCard(sectionPetsTwo, elem));
-// setOfCards[1].forEach(elem => renderCard(sectionPetsTwo, elem));
-// setOfCards[2].forEach(elem => renderCard(sectionPetsTwo, elem));
-// setOfCards[3].forEach(elem => renderCard(sectionPetsTwo, elem));
-
-// leftArr.forEach(elem => renderCardLeft(elem));
-
-// rightArr.forEach(elem => renderCardRight(elem));
-
-
-//Button move animation
-
-// const movePetsright = () => {
-//   couruselPets.classList.add('transition-left-pets');
-//   rightBtnPets.addEventListener('click', movePetsright)
-// }
-
-// rightBtnPets.addEventListener('click', movePetsright)
-
-// couruselPets.addEventListener('animationend', (animationEvent) => {
-//  if(animationEvent.animationName === 'move-left-pets'){
-//   couruselPets.classList.remove('transition-left-pets');
-//  }
-// })
+setOfCards[0].sort(() => Math.random() - 0.5).forEach(elem => renderCard(sectionPetsSeven, elem));
+setOfCards[1].sort(() => Math.random() - 0.5).forEach(elem => renderCard(sectionPetsEight, elem));
+setOfCards[2].sort(() => Math.random() - 0.5).forEach(elem => renderCard(sectionPetsNine, elem));
+setOfCards[3].sort(() => Math.random() - 0.5).forEach(elem => renderCard(sectionPetsTen, elem));
+setOfCards[4].sort(() => Math.random() - 0.5).forEach(elem => renderCard(sectionPetsEleven, elem));
+setOfCards[5].sort(() => Math.random() - 0.5).forEach(elem => renderCard(sectionPetsTwelve, elem));
+setOfCards[0].sort(() => Math.random() - 0.5).forEach(elem => renderCard(sectionPetsThirteen, elem));
+setOfCards[1].sort(() => Math.random() - 0.5).forEach(elem => renderCard(sectionPetsFourteen, elem));
+setOfCards[2].sort(() => Math.random() - 0.5).forEach(elem => renderCard(sectionPetsFifteen, elem));
+setOfCards[3].sort(() => Math.random() - 0.5).forEach(elem => renderCard(sectionPetsSixteen, elem));
 
 let offset = 0;
 let pageNumber = 1;
 
-// const movePetsRight = () => {
-//   offset += 1370;
-//   sliderPets.setAttribute('style', `left: -${offset}px`);
-// }
+//Адаптация слайдера в зависимости о размеров страницы. Не работает без перезагрузки.
+//Если использовать слушать checksize на window, то не работает из за проблем с фиксированным
+//числом пикселей при движении слайдера. Попробовать в %?
+//Добавить min - max
+
+if (window.matchMedia('(min-width: 1231px) and (max-width: 1600px)').matches) {
+
+  rightBtnPets.addEventListener('click', () => {
+    offset += 1233;
+    sliderPets.setAttribute('style', `left: -${offset}px`);
+    pageNumber++;
+    pageCount.textContent = pageNumber;
+  })
+
+  leftBtnPets.addEventListener('click', () => {
+    offset -= 1233;
+    sliderPets.setAttribute('style', `left: -${offset}px`);
+    pageNumber--;
+    pageCount.textContent = pageNumber;
+  })
+
+  firstPageButton.addEventListener('click', () => {
+    offset = 0;
+    sliderPets.setAttribute('style', `left: -${offset}px`);
+    pageNumber = 1;
+    pageCount.textContent = pageNumber;
+  })
+
+  lastPageButton.addEventListener('click', () => {
+    offset = 1233*5;
+    sliderPets.setAttribute('style', `left: -${offset}px`);
+    pageNumber = 6;
+    pageCount.textContent = pageNumber;
+  })
 
 
-// rightBtnPets.addEventListener('click', movePetsRight)
+  //enable and disable buttons
 
-rightBtnPets.addEventListener('click', () => {
-  offset += 1371;
-  sliderPets.setAttribute('style', `left: -${offset}px`);
-  pageNumber++;
-  pageCount.textContent = pageNumber;
-})
+  rightBtnPets.addEventListener('click', () => {
+    if(pageNumber > 1){
+      leftBtnPets.classList.remove('friends__button_type_disabled');
+      firstPageButton.classList.remove('friends__button_type_disabled');
+    } if(pageNumber === 6) {
+      rightBtnPets.classList.add('friends__button_type_disabled');
+      lastPageButton.classList.add('friends__button_type_disabled');
+    }
+  })
 
-leftBtnPets.addEventListener('click', () => {
-  offset -= 1371;
-  sliderPets.setAttribute('style', `left: -${offset}px`);
-  pageNumber--;
-  pageCount.textContent = pageNumber;
-})
+  leftBtnPets.addEventListener('click', () => {
+    if(pageNumber === 1){
+      leftBtnPets.classList.add('friends__button_type_disabled');
+      firstPageButton.classList.add('friends__button_type_disabled');
+    }if(pageNumber < 6){
+      rightBtnPets.classList.remove('friends__button_type_disabled');
+      lastPageButton.classList.remove('friends__button_type_disabled');
+    }
+  })
 
-firstPageButton.addEventListener('click', () => {
-  offset = 0;
-  sliderPets.setAttribute('style', `left: -${offset}px`);
-  pageNumber = 1;
-  pageCount.textContent = pageNumber;
-})
+}
+
+if (window.matchMedia('(min-width: 766px) and (max-width: 1230px)').matches) {
+
+  rightBtnPets.addEventListener('click', () => {
+    offset += 1222;
+    sliderPets.setAttribute('style', `left: -${offset}px`);
+    pageNumber++;
+    pageCount.textContent = pageNumber;
+  })
+
+  leftBtnPets.addEventListener('click', () => {
+    offset -= 1222;
+    sliderPets.setAttribute('style', `left: -${offset}px`);
+    pageNumber--;
+    pageCount.textContent = pageNumber;
+  })
+
+  firstPageButton.addEventListener('click', () => {
+    offset = 0;
+    sliderPets.setAttribute('style', `left: -${offset}px`);
+    pageNumber = 1;
+    pageCount.textContent = pageNumber;
+  })
 
 
-lastPageButton.addEventListener('click', () => {
-  offset = 1371*7;
-  sliderPets.setAttribute('style', `left: -${offset}px`);
-  pageNumber = 8;
-  pageCount.textContent = pageNumber;
-})
+  lastPageButton.addEventListener('click', () => {
+    offset = 1222*7;
+    sliderPets.setAttribute('style', `left: -${offset}px`);
+    pageNumber = 8;
+    pageCount.textContent = pageNumber;
+  })
 
 
-//enable and disable buttons
+  //enable and disable buttons
 
-rightBtnPets.addEventListener('click', () => {
-  if(pageNumber > 1){
-    leftBtnPets.classList.remove('friends__button_type_disabled');
-    firstPageButton.classList.remove('friends__button_type_disabled');
-  } if(pageNumber === 8) {
-    rightBtnPets.classList.add('friends__button_type_disabled');
-    lastPageButton.classList.add('friends__button_type_disabled');
-  }
-})
+  rightBtnPets.addEventListener('click', () => {
+    if(pageNumber > 1){
+      leftBtnPets.classList.remove('friends__button_type_disabled');
+      firstPageButton.classList.remove('friends__button_type_disabled');
+    } if(pageNumber === 8) {
+      rightBtnPets.classList.add('friends__button_type_disabled');
+      lastPageButton.classList.add('friends__button_type_disabled');
+    }
+  })
 
-leftBtnPets.addEventListener('click', () => {
-  if(pageNumber === 1){
-    leftBtnPets.classList.add('friends__button_type_disabled');
-    firstPageButton.classList.add('friends__button_type_disabled');
-  }if(pageNumber < 8){
-    rightBtnPets.classList.remove('friends__button_type_disabled');
-    lastPageButton.classList.remove('friends__button_type_disabled');
-  }
-})
+  leftBtnPets.addEventListener('click', () => {
+    if(pageNumber === 1){
+      leftBtnPets.classList.add('friends__button_type_disabled');
+      firstPageButton.classList.add('friends__button_type_disabled');
+    }if(pageNumber < 8){
+      rightBtnPets.classList.remove('friends__button_type_disabled');
+      lastPageButton.classList.remove('friends__button_type_disabled');
+    }
+  })
 
+}
+
+if (window.matchMedia('(min-width: 320px) and (max-width: 765px)').matches){
+
+  rightBtnPets.addEventListener('click', () => {
+    offset += 330;
+    sliderPets.setAttribute('style', `left: -${offset}px`);
+    pageNumber++;
+    pageCount.textContent = pageNumber;
+  })
+
+  leftBtnPets.addEventListener('click', () => {
+    offset -= 330;
+    sliderPets.setAttribute('style', `left: -${offset}px`);
+    pageNumber--;
+    pageCount.textContent = pageNumber;
+  })
+
+  firstPageButton.addEventListener('click', () => {
+    offset = 0;
+    sliderPets.setAttribute('style', `left: -${offset}px`);
+    pageNumber = 1;
+    pageCount.textContent = pageNumber;
+  })
+
+
+  lastPageButton.addEventListener('click', () => {
+    offset = 330*15;
+    sliderPets.setAttribute('style', `left: -${offset}px`);
+    pageNumber = 16;
+    pageCount.textContent = pageNumber;
+  })
+
+
+  //enable and disable buttons
+
+  rightBtnPets.addEventListener('click', () => {
+    if(pageNumber > 1){
+      leftBtnPets.classList.remove('friends__button_type_disabled');
+      firstPageButton.classList.remove('friends__button_type_disabled');
+    } if(pageNumber === 16) {
+      rightBtnPets.classList.add('friends__button_type_disabled');
+      lastPageButton.classList.add('friends__button_type_disabled');
+    }
+  })
+
+  leftBtnPets.addEventListener('click', () => {
+    if(pageNumber === 1){
+      leftBtnPets.classList.add('friends__button_type_disabled');
+      firstPageButton.classList.add('friends__button_type_disabled');
+    }if(pageNumber < 16){
+      rightBtnPets.classList.remove('friends__button_type_disabled');
+      lastPageButton.classList.remove('friends__button_type_disabled');
+    }
+  })
+
+}
+
+
+
+
+
+//Включение и выключение кнопок при переходе на первую\последнюю страницы.
 firstPageButton.addEventListener('click', () => {
   leftBtnPets.classList.add('friends__button_type_disabled');
   firstPageButton.classList.add('friends__button_type_disabled');
